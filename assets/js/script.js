6de2ea5c87adc275f425d95cd18026c8b0f3cbb8
qulei0316 <634849228@qq.com>
2018-04-26 17:19:43 +0800

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+	/*
+	 * wow
+	 */
+	// new WOW().init();
+
+
+	$('.welcome').backstretch("assets/img/1.jpg");
+
+
+
+
+
+	/*
+	 * background
+	 */
+//	$('.welcome').backstretch("assets/img/1.jpg");
+})
